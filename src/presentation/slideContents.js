db0f4004bfe5331049414a19701aebde3d5def12
Mark Erikson <mark@isquaredsoftware.com>
2019-05-31 15:13:19 -0400

    Initial commit

---
+++ src/presentation/slideContents.js
@@ -0,0 +1,9 @@
+import intro from "./intro/intro.mdx";
+
+import furtherInformation from "./furtherInformation/furtherInformation.mdx";
+
+
+export default [
+    ...intro,
+    ...furtherInformation,
+]
